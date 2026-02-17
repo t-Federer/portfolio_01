aad762b31a4d75b2fa545335a81a592b9f24df71
t-Federer <talaricofederico@outlook.com>
2026-02-17 03:38:45 +0100

    feat: add Experience component with placeholder contents

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
 import Footer from  "./sections/Footer";
+import Experience from "./sections/Experience";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <Experience />
       <Contact />
       <Footer />
     </main>
